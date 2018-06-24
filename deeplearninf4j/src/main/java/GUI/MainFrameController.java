cffb5f03b8163a91fe21ab342cf1f65b11de1085
Aleksey Shiyanov <Shiyanov1998@mail.ru>
2018-06-24 13:22:38 +0400

    add gui

---
+++ deeplearninf4j/src/main/java/GUI/MainFrameController.java
@@ -0,0 +1,33 @@
+package GUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainFrameController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane board;
+
+    @FXML
+    private Label answer;
+
+    @FXML
+    private Button launch;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
